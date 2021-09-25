a6c092f203c25974832ff3002c52ed57ac5ccefe
MuffinJets <hennkensk@gmail.com>
2021-09-25 14:54:51 -0500

    Finished basic item grid and items only layout

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,2 +1,11 @@
-filler()
-end
+local variant = Tracker.ActiveVariantUID
+
+--Things that are loaded no matter what
+
+Tracker:AddItems("items/items.json")
+ScriptHost:LoadScript("scripts/logic.lua")
+Tracker:AddLayouts("items_only/itemsonlylayout.json")
+
+print("Hi!  This is MuffinJets.  If you see any red text above this line, please let me know on Discord or on Guilded!")
+
+--Start with items only variant, add map later
--- scripts/logic.lua
+++ scripts/logic.lua
@@ -1,2 +1,9 @@
-filler()
+function has(item, amount)
+    local count = Tracker:ProviderCountForCode(item)
+    amount = tonumber(amount)
+    if not amount then
+      return count > 0
+    else
+      return count == amount
+    end
 end
